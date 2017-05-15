abc4069f2732a45271f6545fd1e75790c5590ae7
Jordan Wills <jordan.millikin14@gmail.com>
2017-05-15 14:24:23 -0500

    made new repo

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,10 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jordanwills on 5/15/17.
+ */
+@Controller
+public class HelloController {
+}
